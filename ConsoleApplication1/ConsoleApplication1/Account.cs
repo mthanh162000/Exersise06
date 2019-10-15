d470ca616eb31dd3413d69c6d915329d8fdf3a6f
mthanh162000 <lyminhthanh01062000@gmail.com>
2019-10-15 17:51:36 +0700

    update

---
+++ ConsoleApplication1/ConsoleApplication1/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+
+    public class Account
+    {
+        private int money;
+        public Account(int amount)
+        {
+            money = amount;
+        }
+        public void Deposit(int amount)
+        {
+            money += amount;
+        }
+        public void Withdraw(int amount)
+        {
+            money -= amount;
+        }
+        public int Balance()
+        {
+            return money;
+        }
+
+    }
+}
---
+++ ConsoleApplication1/UnitTestProject1/UnintTestAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnintTestAccount
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Account acc = new Account(200000);
+
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(Account));
+            Assert.AreEqual(200000, acc.Balance());
+        }
+    }
+}
