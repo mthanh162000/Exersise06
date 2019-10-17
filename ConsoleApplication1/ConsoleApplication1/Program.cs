d33144dc822c89d7a293fa43e4fc600073dc6054
mthanh162000 <lyminhthanh01062000@gmail.com>
2019-10-17 16:38:35 +0700

    done

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,18 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("money bank");
+            int money = int.Parse(Console.ReadLine());
+            int c, d;
+            Console.WriteLine("money with");
+            int with = int.Parse(Console.ReadLine());
+            c = money - with;
+            Console.WriteLine("Balance " + money + "-" + with + "=" + c);
+            Console.WriteLine("money bank " + c);
+            Console.WriteLine("money Depo ");
+            int Depo = int.Parse(Console.ReadLine());
+            d = c + Depo;
+            Console.WriteLine("Balance " + c + "+" + Depo + "=" + d);
         }
     }
 }
--- ConsoleApplication1/UnitTestProject1/UnintTestAccount.cs
+++ ConsoleApplication1/UnitTestProject1/UnintTestAccount.cs
@@ -15,5 +15,32 @@ namespace UnitTestProject1
             Assert.IsInstanceOfType(acc, typeof(Account));
             Assert.AreEqual(200000, acc.Balance());
         }
+        [TestMethod]
+        public void TestMethodWithDraw()
+        {
+            Account acc = new Account(500000);
+            acc.Withdraw(200000);
+            Assert.AreEqual(300000, acc.Balance());
+            acc.Withdraw(300000);
+            Assert.AreEqual(0, acc.Balance());
+        }
+        [TestMethod]
+        public void TestMethodDeposit()
+        {
+            Account acc = new Account(500000);
+            acc.Deposit(200000);
+            Assert.AreEqual(700000, acc.Balance());
+            acc.Deposit(300000);
+            Assert.AreEqual(1000000, acc.Balance());
+        }
+        [TestMethod]
+        public void TestMethodBalance()
+        {
+            Account acc = new Account(500000);
+            acc.Deposit(200000);
+            Assert.AreEqual(700000, acc.Balance());
+            acc.Deposit(300000);
+            Assert.AreEqual(00000, acc.Balance());
+        }
     }
 }
